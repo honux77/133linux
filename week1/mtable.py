f67528939c17626ee06f5a7e2b6e3267d027a9ca
hosu13 <k502000@naver.com>
2013-10-10 20:43:38 +0900

    week3

--- week1/mtable.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,10):
-	for j in range(1,10):
-		k = i * j
-		print "%2d "%k,
-	print
-
--- week1/query1.sql
+++
@@ -1,3 +0,0 @@
-create database next_db;
-create user 'next'@'localhost' identified by 'next0708';
-grant all privileges on next_db.* TO 'next'@'localhost' WITH GRANT OPTION;
