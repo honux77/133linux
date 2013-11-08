c7432e3e71d6bfed32be76bb3132ba85d5065af6
horong54 <k502000@naver.com>
2013-11-08 17:58:03 +0900

    week7

---
+++ week7/ex1/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int power(int x, int n);
+void tri(int size);
+
+int main() {
+	int x;
+	x = power(2,5);
+	tri(x);
+
+}
+
+int power(int x, int n) {
+	//implement
+}
+
+void tri(int size) {
+	//implement
+	//*
+	//**
+	//***
+	//****
+	//***** 
+}
+
